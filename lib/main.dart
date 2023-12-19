c074d5ebd4534dfa334990ce964e71d37c8a20f2
azizbhutta <azizbhutta90@gmail.com>
2023-12-19 18:44:57 +0500

    chrome web fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,38 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:plannerapp/Aut/login-screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyA0rc8rCeJiEnN36THflcVWkDBiro2CzgM',
+            appId: '1:758640598581:web:94cedf9a48c071e0bb5b7c',
+            messagingSenderId: 'G-B4G98CLEBG',
+            projectId: 'planner-app-9d4a9',
+          storageBucket: 'planner-app-9d4a9.appspot.com',
+        ),);
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  const LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
